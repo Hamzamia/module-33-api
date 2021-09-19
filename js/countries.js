6f29c717c053baecd16c08de360e8d33dc633cd1
Hamzammia <mahmudul.web.developer@gmail.com>
2021-09-19 13:30:08 +0600

    main

--- js/countries.js
+++ js/countries.js
@@ -16,12 +16,34 @@ const displayCountries = countries => {
     countries.forEach(country => {
         const div = document.createElement('div');
         div.classList.add('country');
-        const h3 = document.createElement('h3');
-        h3.innerText = country.name;
-        div.appendChild(h3);
-        const p = document.createElement('p');
-        p.innerText = country.capital;
-        div.appendChild(p);
+        div.innerHTML = `
+<h3>${country.name}</h3>
+<p>${country.capital}</p>
+<button onclick="loadCountryName('${country.name}')">Details</button>
+`
+
+        // const h3 = document.createElement('h3');
+        // h3.innerText = country.name;
+        // div.appendChild(h3);
+        // const p = document.createElement('p');
+        // p.innerText = country.capital;
+        // div.appendChild(p);
         countriesDiv.appendChild(div)
     })
+}
+
+const loadCountryName = name => {
+    const url = `https://restcountries.eu/rest/v2/name/${name}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displaycountryDtail(data[0]));
+}
+
+const displaycountryDtail = country => {
+    const countryDiv = document.getElementById('country-detail');
+    countryDiv.innerHTML = `
+    <h3>${country.name}</h3>
+    <p> Population: ${country.population}</p>
+    <img width=200px src="${country.flag}">
+    `
 }
